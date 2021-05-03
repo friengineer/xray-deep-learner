cfb5bd44265510aa39ed8213c4a49b0a0d3dd2fc
Zapajaka <ll16rdjd@leeds.ac.uk>
2021-05-03 12:37:46 +0100

    Tidied up and commented final version of code. Made final edits to other documents.

--- densenet.py
+++ densenet.py
@@ -6,23 +6,24 @@ from collections import OrderedDict
 
 __all__ = ['DenseNet', 'densenet169']
 
-
 model_urls = {
     'densenet169': 'https://download.pytorch.org/models/densenet169-b2777c0a.pth',
 }
 
 def densenet169(pretrained=False, **kwargs):
     r"""Densenet-169 model from
-    `"Densely Connected Convolutional Networks" <https://arxiv.org/pdf/1608.06993.pdf>`_
+    `"Densely Connected Convolutional Networks" <https://arxiv.org/pdf/1608.06993.pdf>`_.
 
     Args:
         pretrained (bool): If True, returns a model pre-trained on ImageNet
     """
-    model = DenseNet(num_init_features=64, growth_rate=32, block_config=(6, 12, 32, 32),
-                     **kwargs)
+    model = DenseNet(num_init_features=64, growth_rate=32, block_config=(6, 12, 32, 32), **kwargs)
+
     if pretrained:
         model.load_state_dict(model_zoo.load_url(model_urls['densenet169']), strict=False)
+
     return model
+
 
 class _DenseLayer(nn.Sequential):
     def __init__(self, num_input_features, growth_rate, bn_size, drop_rate):
@@ -39,14 +40,17 @@ class _DenseLayer(nn.Sequential):
 
     def forward(self, x):
         new_features = super(_DenseLayer, self).forward(x)
+
         if self.drop_rate > 0:
             new_features = F.dropout(new_features, p=self.drop_rate, training=self.training)
+
         return torch.cat([x, new_features], 1)
 
 
 class _DenseBlock(nn.Sequential):
     def __init__(self, num_layers, num_input_features, bn_size, growth_rate, drop_rate):
         super(_DenseBlock, self).__init__()
+
         for i in range(num_layers):
             layer = _DenseLayer(num_input_features + i * growth_rate, growth_rate, bn_size, drop_rate)
             self.add_module('denselayer%d' % (i + 1), layer)
@@ -80,7 +84,7 @@ class DenseNet(nn.Module):
 
         super(DenseNet, self).__init__()
 
-        # First convolution
+        # first convolution
         self.features = nn.Sequential(OrderedDict([
             ('conv0', nn.Conv2d(3, num_init_features, kernel_size=7, stride=2, padding=3, bias=False)),
             ('norm0', nn.BatchNorm2d(num_init_features)),
@@ -88,28 +92,27 @@ class DenseNet(nn.Module):
             ('pool0', nn.MaxPool2d(kernel_size=3, stride=2, padding=1)),
         ]))
 
-        # Each denseblock
+        # each denseblock
         num_features = num_init_features
+
         for i, num_layers in enumerate(block_config):
             block = _DenseBlock(num_layers=num_layers, num_input_features=num_features,
                                 bn_size=bn_size, growth_rate=growth_rate, drop_rate=drop_rate)
             self.features.add_module('denseblock%d' % (i + 1), block)
             num_features = num_features + num_layers * growth_rate
+
             if i != len(block_config) - 1:
                 trans = _Transition(num_input_features=num_features, num_output_features=num_features // 2)
                 self.features.add_module('transition%d' % (i + 1), trans)
                 num_features = num_features // 2
 
-        # Final batch norm
+        # final batch norm
         self.features.add_module('norm5', nn.BatchNorm2d(num_features))
-        
-        # Linear layer
-        # self.classifier = nn.Linear(num_features, 1000)
-        # self.fc = nn.Linear(1000, 1)
-        
+
+        # linear layer
         self.fc = nn.Linear(num_features, 1)
-        
-        # Official init from torch repo.
+
+        # official init from torch repo
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
                 nn.init.kaiming_normal(m.weight.data)
@@ -123,6 +126,6 @@ class DenseNet(nn.Module):
         features = self.features(x)
         out = F.relu(features, inplace=True)
         out = F.avg_pool2d(out, kernel_size=7, stride=1).view(features.size(0), -1)
-        # out = F.relu(self.classifier(out))
         out = F.sigmoid(self.fc(out))
+
         return out
